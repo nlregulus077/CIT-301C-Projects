f9f2daf7d44f89ccda56eef5440a4a029984eed6
Jeanina Lao <lao13001@byui.edu>
2017-05-19 13:50:34 -0600

    5/19/2017 1:50 - document-list component updated

--- cms/src/app/documents/documents-list/documents-list.component.ts
+++ cms/src/app/documents/documents-list/documents-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-documents-list',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentsListComponent implements OnInit {
 
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] =
+    [
+      new Document(1, 'Materia', 'A detailed list of all known materia found in the Planet', null, null),
+      new Document(2, 'GF', 'A comprehensive list of all known Guardian Forces found in the Balamb region', null, null),
+      new Document(3, 'Eidolons', 'Ancient eidolon summoning chants used by ancient summoners', null, null),
+      new Document(4, 'Aeons', 'A brief history of the Fayth and Aeons', null, null),
+      new Document(5, 'Ragnarok', 'A detailed account of when Ragnarok penetrated Cocoon\'s outer shell', null, null),
+    ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
